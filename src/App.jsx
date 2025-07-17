79ac5c26066df61f9b518fa13a3556e00e7a735e
Umar-Farooq-5757 <aamirgel17@gmail.com>
2025-07-17 17:40:31 +0500

    Used uiverse.io in footer

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import "./App.css";
 import Header from "./components/Header";
 import ResizingButtons from "./components/ResizingButtons";
 import Main from "./components/Main";
+import Footer from "./components/Footer";
 
 function App() {
   // const [isDark, setIsDark] = useState(true);
@@ -12,6 +13,7 @@ function App() {
       <Header />
       <ResizingButtons />
       <Main />
+      <Footer />
     </main>
   );
 }
